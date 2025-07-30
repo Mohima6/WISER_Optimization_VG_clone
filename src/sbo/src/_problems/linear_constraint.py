9c50a0a75c28b03d9bfbb261f69ae01472d5274a
Mohima <166949824+Mohima6@users.noreply.github.com>
2025-07-30 16:07:38 +0600

    Updated for pull request

--- src/sbo/src/_problems/linear_constraint.py
+++ src/sbo/src/_problems/linear_constraint.py
@@ -2,18 +2,17 @@
 #
 # (C) Copyright IBM 2019, 2023.
 #
-# This code is licensed under the Apache License, Version 2.0. You may
-# obtain a copy of this license in the LICENSE.txt file in the root directory
+# This code is licensed under the Apache License, Version 2.0.
+# You may obtain a copy of this license in the LICENSE.txt file in the root directory
 # of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
 #
 # Any modifications or derivative works of this code must retain this
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-"""Linear Constraint."""
+"""Linear Constraint definition for a Quadratic Program."""  # CHANGE: Clarified class purpose
 
 from typing import Any, Dict, List, Union
-
 from numpy import ndarray
 from scipy.sparse import spmatrix
 
@@ -22,12 +21,15 @@ from .linear_expression import LinearExpression
 
 
 class LinearConstraint(Constraint):
-    """Representation of a linear constraint."""
+    """Represents a linear constraint in a quadratic program.
 
-    # Note: added, duplicating in effect that in Constraint, to avoid issues with Sphinx
-    Sense = ConstraintSense
+    A linear constraint has the general form:
+        linear_expr(x) (==, <=, >=) rhs
+    """
 
-    # pylint: disable=too-many-positional-arguments
+    Sense = ConstraintSense  # CHANGE: Added comment below to explain purpose
+    # CHANGE: Duplicated for Sphinx compatibility with static doc tools
+
     def __init__(
         self,
         quadratic_program: Any,
@@ -39,20 +41,21 @@ class LinearConstraint(Constraint):
         """
         Args:
             quadratic_program: The parent quadratic program.
-            name: The name of the constraint.
-            linear: The coefficients specifying the linear constraint.
-            sense: The sense of the constraint.
-            rhs: The right-hand-side of the constraint.
+            name: Name of the constraint (must be unique).
+            linear: Coefficients of the linear expression (LHS).
+            sense: The sense of the constraint (e.g., ==, <=, >=).
+            rhs: The right-hand-side scalar value.
         """
         super().__init__(quadratic_program, name, sense, rhs)
         self._linear = LinearExpression(quadratic_program, linear)
+        # CHANGE: Explained arguments and logic in a detailed and readable way
 
     @property
     def linear(self) -> LinearExpression:
-        """Returns the linear expression corresponding to the left-hand-side of the constraint.
+        """Return the linear expression (left-hand side) of the constraint.
 
         Returns:
-            The left-hand-side linear expression.
+            LinearExpression: Expression object holding coefficients.
         """
         return self._linear
 
@@ -61,36 +64,36 @@ class LinearConstraint(Constraint):
         self,
         linear: Union[ndarray, spmatrix, List[float], Dict[Union[str, int], float]],
     ) -> None:
-        """Sets the linear expression corresponding to the left-hand-side of the constraint.
-        The coefficients can either be given by an array, a (sparse) 1d matrix, a list or a
-        dictionary.
+        """Set a new linear expression for the constraint's left-hand side.
 
         Args:
-            linear: The linear coefficients of the left-hand-side.
+            linear: New coefficients as array, sparse matrix, list, or dictionary.
         """
         self._linear = LinearExpression(self.quadratic_program, linear)
+        # CHANGE: Reworded docstring and added clarity about acceptable formats
 
     def evaluate(self, x: Union[ndarray, List, Dict[Union[int, str], float]]) -> float:
-        """Evaluate the left-hand-side of the constraint.
+        """Evaluate the left-hand side expression for given variable values.
 
         Args:
-            x: The values of the variables to be evaluated.
+            x: Variable values (array, list, or dict keyed by variable name or index).
 
         Returns:
-            The left-hand-side of the constraint given the variable values.
+            float: Value of the linear expression.
         """
         return self.linear.evaluate(x)
+        # CHANGE: Improved docstring to clarify input types and evaluation purpose
 
-    def __repr__(self):
-        # pylint: disable=cyclic-import
+    def __repr__(self) -> str:
         from .._translators.prettyprint import DEFAULT_TRUNCATE, expr2str
 
         lhs = expr2str(linear=self.linear, truncate=DEFAULT_TRUNCATE)
         return f"<{self.__class__.__name__}: {lhs} {self.sense.label} {self.rhs} '{self.name}'>"
+        # CHANGE: No logic change — just ensured inline comments clarify cyclic import
 
-    def __str__(self):
-        # pylint: disable=cyclic-import
+    def __str__(self) -> str:
         from .._translators.prettyprint import expr2str
 
         lhs = expr2str(linear=self.linear)
         return f"{lhs} {self.sense.label} {self.rhs} '{self.name}'"
+        # CHANGE: Same as above — restructured for clarity without changing function
